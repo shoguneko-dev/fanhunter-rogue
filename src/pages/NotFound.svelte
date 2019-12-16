759125ba03576336495b1c801c8063b662cca81b
omirobarcelo <shogunekodev@gmail.com>
2019-12-16 19:49:49 +0100

    Implemented 404 page

--- src/pages/NotFound.svelte
+++ src/pages/NotFound.svelte
@@ -1,1 +1,21 @@
-<h2>404</h2>
+<style type="text/scss">
+  $font-size: 15rem;
+
+  h1,
+  h2 {
+    font-family: "Nova Mono", monospace;
+    margin: 0;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: $font-size;
+  }
+
+  h2 {
+    font-size: calc(#{$font-size}/ 5);
+  }
+</style>
+
+<h1>404</h1>
+<h2>Page Not Found</h2>
