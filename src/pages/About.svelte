69ae5abe39e29a5dad63c44f24792a9202aac044
omirobarcelo <shogunekodev@gmail.com>
2019-12-16 22:16:23 +0100

    Initial implementation of About page

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,1 +1,20 @@
-<h2>About</h2>
+<style type="text/scss">
+  .container {
+    padding: 1rem 2rem;
+  }
+</style>
+
+<div class="container">
+  <h2>Goal</h2>
+
+  <p>The goal of the game is to do some stuff</p>
+
+  <h2>Commands</h2>
+
+  <ul>
+    <li>
+      <b>toggle</b>
+      -- Toggles map on/off
+    </li>
+  </ul>
+</div>
